0f891c68fe954901ce3109e1824c7aa02164dd90
Serkan Akçay <serkanakcayyy@gmail.com>
2024-11-29 18:08:47 +0300

    Initial Commit

---
+++ netflix-clone-ios-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by serkan on 29.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
