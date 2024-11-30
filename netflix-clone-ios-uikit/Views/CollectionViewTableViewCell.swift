d680510629d04be5814cdc6f1ccb5f793f02920c
Serkan Akçay <serkanakcayyy@gmail.com>
2024-11-30 16:36:24 +0300

    HomeFeedTable Created

---
+++ netflix-clone-ios-uikit/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by serkan on 30.11.2024.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
