9331c6fc28399c776d157f694c7a4f3b179c05fd
Serkan Akçay <serkanakcayyy@gmail.com>
2024-12-05 22:39:58 +0300

    Table Header View

---
+++ netflix-clone-ios-uikit/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by serkan on 5.12.2024.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
