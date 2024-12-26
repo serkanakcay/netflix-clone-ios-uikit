23c77a55fef3fa6f4facbda1569d76a3e395460b
Serkan Akçay <serkanakcayyy@gmail.com>
2024-12-26 17:08:03 +0300

    HeroHeaderView

--- netflix-clone-ios-uikit/Views/HeroHeaderUIView.swift
+++ netflix-clone-ios-uikit/Views/HeroHeaderUIView.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 class HeroHeaderUIView: UIView {
+    
+    private let downLoadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let playButton: UIButton = {
+    let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
     private var heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -33,7 +53,32 @@ class HeroHeaderUIView: UIView {
         super.init(frame: frame)
         addSubview(heroImageView)
         addGradient()
+        addSubview(playButton)
+        addSubview(downLoadButton)
+        applyConstraints()
+
     }
+    
+    private func applyConstraints() {
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 70), //  leadingAnchor kullanıldı
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        let downloadButtonConstaints = [
+            downLoadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -70),
+            downLoadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downLoadButton.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        NSLayoutConstraint.activate(playButtonConstraints)
+        NSLayoutConstraint.activate(downloadButtonConstaints)
+    }
+    
+    
+    
+    
+    
     override func layoutSubviews() {
         heroImageView.frame = bounds
         
