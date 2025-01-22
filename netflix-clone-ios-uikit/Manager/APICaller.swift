9d3d9e5ab11a914da3a129f549707fdc9ea60917
Serkan Akçay <serkanakcayyy@gmail.com>
2025-01-22 14:16:56 +0300

    APICALLER

---
+++ netflix-clone-ios-uikit/Manager/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by serkan on 17.01.2025.
+//
+
+import Foundation
---
+++ netflix-clone-ios-uikit/Models/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by serkan on 22.01.2025.
+//
+
+import Foundation
