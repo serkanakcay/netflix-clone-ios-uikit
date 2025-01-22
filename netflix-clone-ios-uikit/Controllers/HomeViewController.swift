26473667dc368f67553b450a4f830d1dfece879e
Serkan Akçay <serkanakcayyy@gmail.com>
2025-01-22 14:17:17 +0300

    apicaller

--- netflix-clone-ios-uikit/Controllers/HomeViewController.swift
+++ netflix-clone-ios-uikit/Controllers/HomeViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    
+    let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Top Rated", "Upcoming Movies", "Popular"]
+    
+    
+    
     private let homeFeedTableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -20,21 +26,52 @@ class HomeViewController: UIViewController {
         view.addSubview(homeFeedTableView)
         homeFeedTableView.delegate = self
         homeFeedTableView.dataSource = self
-        
+        configureNavbar()
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTableView.tableHeaderView = headerView
-
+        getTrendingMovies()
     }
+    
+    private func configureNavbar() {
+        let logoImageView = UIImageView(image: UIImage(named: "netflixLogo")?.withRenderingMode(.alwaysOriginal))
+        logoImageView.contentMode = .scaleAspectFit
+        
+        let logoContainerView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+        logoImageView.frame = logoContainerView.bounds
+        logoContainerView.addSubview(logoImageView)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoContainerView)
+        
+        let personButton = UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil)
+        let playButton = UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        
+        
+        navigationItem.rightBarButtonItems = [personButton, playButton]
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTableView.frame = view.bounds
     }
+    private func getTrendingMovies() {
+        APICaller.shared.getTrendingMovies { result in
+            switch result {
+            case .success(let movies):
+                print("Movies: \(movies)")
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    
+    }
 }
-
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+        return sectionTitles.count
     }
     
    
@@ -59,5 +96,27 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
             return 40
         }
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else {return}
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .white
+        header.textLabel?.text = header.textLabel?.text?.lowercased()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+        
+    }
+    
     
 }
--- netflix-clone-ios-uikit/Manager/APICaller.swift
+++ netflix-clone-ios-uikit/Manager/APICaller.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+struct Constanst{
+    static let API_KEY = "331e93728a505ed448bf74d2a52933fd"
+    static let baseURL = "https://api.themoviedb.org"
+}
+enum APIError: Error{
+    case failedTogetData
+}
+
+class APICaller{
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void){
+        guard let url = URL(string: "\(Constanst.baseURL)/3/trending/all/day?api_key=\(Constanst.API_KEY)")else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)){data, _, error in
+            guard let data = data, error == nil else{
+                return
+            }
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+}
