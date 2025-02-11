00ba6f0e49ecbbbbb39bb2f270c348648ba5d4aa
Serkan Akçay <serkanakcayyy@gmail.com>
2025-02-11 14:14:58 +0300

    Upcoming

---
+++ netflix-clone-ios-uikit/ViewModels/TitleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TitleViewModel.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by serkan on 10.02.2025.
+//
+
+import Foundation
---
+++ netflix-clone-ios-uikit/Views/TitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleTableViewCell.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by serkan on 10.02.2025.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
